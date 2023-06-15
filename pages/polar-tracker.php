661489abc0968db9ac70ab4c194ca20d5febaf8d
zdeep10 <gabriel.wti02@gmail.com>
2023-06-14 22:47:52 -0300

    Prototipo Grosseiro polar-tracker 1/2

--- pages/polar-tracker.php
+++ pages/polar-tracker.php
@@ -4,7 +4,7 @@ require $root . '/partials/header.php';
 ?>
 
 <!-- GERENCIAMENTO -->
-<!-- <section id="gerencimanto">
+<section id="gerencimanto">
     <div class="container px-4">
         <div class="row">
             <div class="col">
@@ -13,10 +13,10 @@ require $root . '/partials/header.php';
             </div>
         </div>
     </div>
-</section> -->
+</section>
 
 <!-- SOLUÇÕES -->
-<!-- <section id="solucoes">
+<section id="solucoes">
     <div class="container">
         <div class="row">
             <div class="col-12">
@@ -163,18 +163,128 @@ require $root . '/partials/header.php';
             </div>
         </div>
     </div>
-</section> -->
+</section>
 
 <!-- PACOTE -->
 <section id="pacote">
     <div class="container px-4">
-        <div class="row">
-            <div class="col">
+        <!-- <div class="row">
+            <div class="col-12">
                 <h2>Pacote de soluções</h2>
             </div>
 
             <div class="col">
-                
+                <div class="card text-center" style="width: 18rem;">
+                    <div class="bg-secondary border-circle w-25 mx-auto text-white fw-bold">1</div>
+                    <img src="/assets/icons/aws_icon.png" width="70" height="52" class="img-fluid mx-auto" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Plataforma</h5>
+                        <p class="card-text">Hospedagem em Nuvem
+                            Validação
+                            Intuitiva e Amigável</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card text-center" style="width: 18rem;">
+                    <div class="bg-secondary border-circle w-25 mx-auto text-white fw-bold">2</div>
+                    <img src="/assets/icons/dispositivos_icon.png" width="26" height="34" class="img-fluid mx-auto" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Dispositivos</h5>
+                        <p class="card-text">Validado
+                            Temperatura
+                            Umidade
+                            Geolocalização
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card text-center" style="width: 18rem;">
+                    <div class="bg-secondary border-circle w-25 mx-auto text-white fw-bold">3</div>
+                    <div class="card-body">
+                        <h5 class="card-title">Instalação & Treinamento</h5>
+                        <p class="card-text">Site Survey(Avaliação do local)
+                            Padronizada
+                            Certificada
+                            Manual de Operação</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card text-center" style="width: 18rem;">
+                    <div class="bg-secondary border-circle w-25 mx-auto text-white fw-bold">4</div>
+                    <div class="card-body">
+                        <h5 class="card-title">Suporte</h5>
+                        <p class="card-text">Padronizado
+                            POP de operação
+                            SLA(Acordo de Nível de Serviço)</p>
+                    </div>
+                </div>
+            </div>
+        </div> -->
+
+        <div class="row">
+            <div class="col-12">
+                <h2>Dashboard</h2>
+                <p>Visualização online e em tempo real das informações essenciais para a sua operação.</p>
+            </div>
+
+            <figure class="col-12 mb-0">
+                <img loading="lazy" src="/assets/images/video-dash-polar-tracker.png" class="img-fluid" alt="Video Thumb">
+            </figure>
+        </div>
+
+        <div class="row">
+            <div class="col-12">
+                <h2>Polar Tracker Light</h2>
+            </div>
+
+            <div class="col-12">
+                <div class="card border-0 border-bottom">
+                    <img loading="lazy" src="/assets/icons/sensor_light_icon.png" width="47" height="47" class="img-fluid mx-auto" alt="Sensor de temperatura icone">
+                    <div class="card-body">
+                        <p class="card-text">Sensor de temperatura de -80ºC até +70ºC
+                            Sensor de umidade na faixa de 10% a 95%</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12">
+                <div class="card border-0 border-bottom">
+                    <img loading="lazy" src="/assets/icons/memoria_light_icon.png" width="35" height="35" class="img-fluid mx-auto" alt="Memória dispositivo icone">
+                    <div class="card-body">
+                        <p class="card-text">Memória no dispositivo reduntante para 30 dias de acordo com CFR21</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12">
+                <div class="card border-0 border-bottom">
+                    <img loading="lazy" src="/assets/icons/bateria_light_icon.png" width="20" height="20" class="img-fluid mx-auto" alt="Bateria Icone">
+                    <div class="card-body">
+                        <p class="card-text">Bateria de no mínimo 15 dias de autonomia e dashboard com alarme visual</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12">
+                <div class="card border-0 border-bottom">
+                    <img loading="lazy" src="/assets/icons/wpp_light_icon.png" width="28" height="28" class="img-fluid mx-auto" alt="Wpp Icone">
+                    <div class="card-body">
+                        <p class="card-text">Envio de avisos por email e Whatsapp e comunicação Wi-Fi e GSM
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12">
+                <div class="card border-0 border-bottom">
+                    <img loading="lazy" src="/assets/icons/calibracao_light_icon.png" width="25" height="25" class="img-fluid mx-auto" alt="Laboratorio acreditado icone">
+                    <div class="card-body">
+                        <p class="card-text">Calibração com laboratório acreditado RBC/INMETRO</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
