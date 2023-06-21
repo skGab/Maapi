a81084d7d583926ad32d0774b9a9253e04917aab
zdeep10 <gabriel.wti02@gmail.com>
2023-06-21 09:25:06 -0300

    Prototipo grosseiro contato

---
+++ pages/contato.php
@@ -0,0 +1,18 @@
+<?php
+$root = $_SERVER['DOCUMENT_ROOT'];
+require $root . '/partials/header.php';
+?>
+
+<!-- COMPONENTE CONTATO -->
+<section id="">
+    <div class="container px-4 py-5">
+        <div class="row">
+            <div class="col"></div>
+        </div>
+    </div>
+</section>
+
+<?php
+$root = $_SERVER['DOCUMENT_ROOT'];
+require $root . '/partials/footer.php';
+?>
