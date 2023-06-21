bea1626a374788f00c5ef4802fe5b803379f1bad
zdeep10 <gabriel.wti02@gmail.com>
2023-06-21 09:23:20 -0300

    Prototipo grosseiro aplicações

--- conteudo/main_banner.php
+++ conteudo/main_banner.php
@@ -5,16 +5,26 @@ function conteudo($page)
     if ($page === "/") {
         echo '
         <h1>Somos a MAAPI</h1>
-        <P>Uma empresa pioneira e inovadora com soluções de ponta para o monitoramento de produtos e cargas em tempo real</P>';
+        <p>Uma empresa pioneira e inovadora com soluções de ponta para o monitoramento de produtos e cargas em tempo real</p>';
     }
     if ($page === "/pages/sobre-nos.php") {
         echo '
         <h1>Sobre nós</h1>
-        <P>Segurança e eficácia no monitoramento online em tempo real da umidade e temperatura</P>';
+        <p>Segurança e eficácia no monitoramento online em tempo real da umidade e temperatura</p>';
     }
     if ($page === "/pages/polar-tracker.php") {
         echo '
         <h1>Polar Tracker</h1>
-        <P>Sistema de gerenciamento para a Cadeia Fria</P>';
+        <p>Sistema de gerenciamento para a Cadeia Fria</p>';
+    }
+    if ($page === "/pages/aplicacoes.php") {
+        echo '
+        <h1>Aplicações</h1>
+        <p>Evite prejuízos! Não corra o risco de perder seus produtos e cargas: com o Polar Tracker é possível monitorar a umidade e temperatura de produtos e cargas em tempo real!</p>';
+    }
+    if ($page === "/pages/contato.php") {
+        echo '
+        <h1>Contato</h1>
+        <p>Entre em contato conosco e fale diretamente com nossos especialistas!</p>';
     }
 }
---
+++ pages/aplicacoes.php
@@ -0,0 +1,88 @@
+<?php
+$root = $_SERVER['DOCUMENT_ROOT'];
+require $root . '/partials/header.php';
+?>
+
+<!-- GALERIA -->
+<section id="galeria">
+    <div class="container px-4 py-5">
+        <div class="row">
+            <div class="col-6">
+                <div class="card border-0 text-center">
+                    <figure>
+                        <img loading="lazy" class="img-fluid" src="../assets/images/thumb_medicina.png" alt="Medicina Thumb">
+                    </figure>
+                    <p>MEDICINA</p>
+                </div>
+            </div>
+
+            <div class="col-6">
+                <div class="card border-0 text-center">
+                    <figure>
+                        <img loading="lazy" class="img-fluid" src="../assets/images/thumb_diagnostico.png" alt="Diagnóstico Thumb">
+                    </figure>
+                    <p>Pesquisa e Diagnóstico</p>
+                </div>
+            </div>
+            
+            <div class="col-6">
+                <div class="card border-0 text-center">
+                    <figure>
+                        <img loading="lazy" class="img-fluid" src="../assets/images/thumb_saude-animal.png" alt="Saúde animal Thumb">
+                    </figure>
+                    <p>Saúde Animal</p>
+                </div>
+            </div>
+            
+            <div class="col-6">
+                <div class="card border-0 text-center">
+                    <figure>
+                        <img loading="lazy" class="img-fluid" src="../assets/images/thumb_logistica.png" alt="Logística Thumb">
+                    </figure>
+                    <p>Logística</p>
+                </div>
+            </div>
+            
+            <div class="col-6">
+                <div class="card border-0 text-center">
+                    <figure>
+                        <img loading="lazy" class="img-fluid" src="../assets/images/thumb_farmaceutica.png" alt="Indústria Thumb">
+                    </figure>
+                    <p>Indústria Farmacêutica</p>
+                </div>
+            </div>
+            
+            <div class="col-6">
+                <div class="card border-0 text-center">
+                    <figure>
+                        <img loading="lazy" class="img-fluid" src="../assets/images/thumb_tranposte.png" alt="Transporte Thumb">
+                    </figure>
+                    <p>Transporte e Armazenamento de Vacinas</p>
+                </div>
+            </div>
+            
+            <div class="col-6">
+                <div class="card border-0 text-center">
+                    <figure>
+                        <img loading="lazy" class="img-fluid" src="../assets/images/thumb_food-service.png" alt="Food service Thumb">
+                    </figure>
+                    <p>Food Service</p>
+                </div>
+            </div>
+            
+            <div class="col-6">
+                <div class="card border-0 text-center">
+                    <figure>
+                        <img loading="lazy" class="img-fluid" src="../assets/images/thumb_camara.png" alt="Câmara Thumb">
+                    </figure>
+                    <p>Câmaras Frias</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+$root = $_SERVER['DOCUMENT_ROOT'];
+require $root . '/partials/footer.php';
+?>
